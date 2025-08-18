eed855179c139ed3172064e0ab8715ebeba71285
RAIMUNDO GOMES DOS SANTOS NETO <gomes.idb@gmail.com>
2025-08-18 20:47:55 -0300

    msg

--- index.js
+++ index.js
@@ -1,34 +1,88 @@
 const express = require('express')
+const cors = require('cors')
+const { PrismaClient } = require('./src/generated/prisma')
+const prisma = new PrismaClient()
 const app = express()
-const port = 3002
+const port = 3001
+
+
+app.use(cors())
+app.use(express.json());
 
 let cars = []
 
 
-app.get('/cars', (req, res) => {
+app.get('/cars', async (req, res) => {
     //   ira retornar uma lista de carros com status 200
 
-    res.status(200).json(cars)
+   const allCars = await prisma.car.findMany()
+
+    res.status(200).json(allCars)
 })
 
-app.post('/cars', (req, res) => {
+app.post('/cars', async (req, res) => {
+    console.log(req.body)
     const car = {
-        name: "Song Plus",
-        category: "SUV",
-        seat: 5,
-        price: 250000,
-        transmission: "automatic",
-        fuel: "electric",
-        imageUrl: "",
-        description: "Um carro eletrico moderno e espaçoso.",
-        features: ["eletrico"]
+        name: req.body.name,
+        image: req.body.image,
+        price: req.body.price,
+        category: req.body.category,
+        transmission: req.body.transmission,
+        fuel: req.body.fuel,
+        seats: req.body.seats,
+        available: req.body.available
     }
 
-    //   ira adicionar um carro com status 201
-    cars.push(car)
-    res.status(201).json(car)
+    const newCar = await prisma.car.create({
+        data: car
+    })
+
+    res.status(201).json(newCar)
+})
+
+app.delete('/cars/:id', async (req, res) => {
+   try{
+       const { id } = req.params
+
+    await prisma.car.delete({
+        where: { id }
+    })
+
+   }catch (error) {
+       console.error(error)
+       res.status(404).json({ error: 'Car not found' })
+   }
+    res.status(204).send()  
+}) 
+
+
+app.put('/cars/:id', async (req, res) => {
+    try {
+        const updatedCar = await prisma.car.update({
+            where: {
+                id: req.params.id
+            },
+            data: {
+                name: req.body.name,
+                category: req.body.category,
+                seats: req.body.seats,
+                price: req.body.price,
+                transmission: req.body.transmission,
+                fuel: req.body.fuel,
+                image: req.body.image,
+                available: req.body.available,
+            }
+        })
+
+        res.status(200).json(updatedCar)
+    } catch (error) {
+        console.error(error)
+        res.status(404).json({ error: 'Car not found' })
+    }
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
